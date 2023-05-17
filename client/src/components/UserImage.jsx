304bae401ba88f334ffdc23c3b167657b0abb2cb
MrStark1234 <117528760+MrStark1234@users.noreply.github.com>
2023-05-17 15:55:07 +0530

    applying server url1

--- client/src/components/UserImage.jsx
+++ client/src/components/UserImage.jsx
@@ -8,7 +8,7 @@ const UserImage = ({ image, size = "60px" }) => {
         width={size}
         height={size}
         alt="user"
-        src={`http://localhost:5000/assets/${image}`}
+        src={`https://social-media-server-tnjz.onrender.com/assets/${image}`}
       />
     </Box>
   );
