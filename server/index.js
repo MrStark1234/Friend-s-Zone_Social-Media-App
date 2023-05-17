48728083270b41658542e1aa631fe4180b2c0a97
MrStark1234 <117528760+MrStark1234@users.noreply.github.com>
2023-05-18 00:27:09 +0530

    check the cloudinaryv1

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ import { verifyToken } from "./middleware/auth.js";
 import User from "./models/User.js";
 import Post from "./models/Post.js";
 import { users, posts } from "./data/index.js";
-const cloudinary = require("cloudinary").v2;
+import cloudinary from "cloudinary";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
